847d2e3227c1cee9d9737a4b54291d3b368a71ef
bowenzhaosh <zbw0202@yeah.net>
2026-04-17 23:42:15 -0500

    Update rlpd_config.py

--- configs/rlpd_config.py
+++ configs/rlpd_config.py
@@ -1,17 +1,9 @@
 from configs import sac_config
-from configs import sac_config
+from ml_collections.config_dict import config_dict
 def get_config():
     config = sac_config.get_config()
     config.num_qs = 10
     config.num_min_qs = 2
     config.critic_layer_norm=True
-    return config
-
-def get_config():
-    config = sac_config.get_config()
-
-    config.num_qs = 10
-    config.num_min_qs = 2
-    config.critic_layer_norm=True
-
+    config.spec_norm_coef = config_dict.placeholder(float)
     return config
